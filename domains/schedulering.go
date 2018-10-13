077a5049fb58adfb76d35f528c7a791ec8889cb2
danilda <danil1996-96@mai.ru>
2018-10-13 12:23:50 +0300

    added base domains and started usecases

---
+++ domains/schedulering.go
@@ -0,0 +1,58 @@
+package domains
+
+import "errors"
+
+//todo it's mock for development
+const (
+	PROFFESSIONAL_TYPE_LAWYERS = iota
+)
+
+type SchedulerRepository interface {
+	FindById(id uint64) Scheduler
+	FindAllByUserId(userId uint64) []Scheduler
+	Store(scheduler Scheduler)
+	Update(scheduler Scheduler)
+}
+
+type IntervalRepository interface {
+	FindById(id uint64) Interval
+	FindAllBySchedulerId(schedulerId uint64, sortBy string) []Interval
+	Store(interval Interval)
+	Update(interval Interval)
+}
+
+type Scheduler struct {
+	Id             uint64
+	UserId         uint64
+	ProfessionType string
+}
+
+type Interval struct {
+	Id          uint64
+	SchedulerId uint64
+	From        uint16 // to
+	To          uint16 // to
+	Date        uint64 // date for exceptional situation: overriding default interval
+	weekDay     uint8
+}
+
+func InitInterval(schedulerId, Date uint64, from, to uint16, weekDay uint8) (Interval, error) {
+	item := Interval{SchedulerId: schedulerId, From: from, To: to, Date: Date}
+	if err := item.SetWeekDay(weekDay); err != nil {
+		return item, err
+	}
+
+	return item, nil
+}
+
+func (item *Interval) SetWeekDay(weekDay uint8) error {
+	if weekDay < 1 || weekDay > 7 {
+		return errors.New("Illegal week weekDay number!")
+	}
+	item.weekDay = weekDay
+	return nil
+}
+
+func (item *Interval) GetWeekDay() uint8 {
+	return item.weekDay
+}
---
+++ usecases/schedulering.go
@@ -0,0 +1,82 @@
+package usecases
+
+import (
+	"errors"
+	"yurko/domains"
+)
+
+type SchedulingInteractor struct {
+	SchedulerRepository domains.SchedulerRepository
+	IntervalRepository  domains.IntervalRepository
+	//Logger
+}
+
+type Time struct {
+	timeInMinutes uint16
+}
+
+type TimeRange struct {
+	start Time
+	end   Time
+}
+
+type Day struct {
+	day    uint8
+	ranges []TimeRange
+}
+
+func InitTime(hours, minutes int8) (Time, error) {
+	if 0 < hours || hours > 23 {
+		return Time{}, errors.New("Invalid hours value [" + string(hours) + "]")
+	} else if 0 < minutes || minutes > 59 {
+		return Time{}, errors.New("Invalid minutes value [" + string(minutes) + "]")
+	}
+	return Time{timeInMinutes: uint16(hours*60 + minutes)}, nil
+}
+
+func InitTimeRange(start, end Time) (TimeRange, error) {
+	if end.compare(start) < 1 {
+		return TimeRange{}, errors.New("Invalid time range. End time not later then start time!")
+	}
+	return TimeRange{start: start, end: end}, nil
+}
+
+func InitDay(day uint8, ranges []TimeRange) (Day, error) {
+	if 1 < day || day > 7 {
+		return Day{}, errors.New("Invalid day number [" + string(day) + "]")
+	}
+	return Day{day: day, ranges: ranges}, nil
+}
+
+func (current *Time) Compare(compared Time) int8 {
+	if current.timeInMinutes > compared.timeInMinutes {
+		return 1
+	} else if current.timeInMinutes < compared.timeInMinutes {
+		return -1
+	}
+	return 0
+}
+
+//
+//*добавление времени работы по умолчанию в профайле
+// а занчит, нужно созать для пользователя (userId) рассписание по
+// значениям [day: 1, ranges: [[start:12:00, end:13:00],[],[]]]
+//*быстрая модификация указанного времени
+// schedulerId + [date: 21.12.1996, ranges: [[start:12:00, end:13:00],[],[]]]
+//*получение рассписания на по дате
+// schedulerId, date
+//- получение рассписания на неделю, а лучше на слайс дат
+func (interactor *SchedulingInteractor) createNewScheduler(userId uint64, professionType string, days []Day) error {
+	scheduler := domains.Scheduler{UserId: userId, ProfessionType: professionType}
+	intervals := make([]domains.Interval, 0, 14)
+	for _, day := range days {
+		for _, timeRange := range day.ranges {
+			start := timeRange.start.timeInMinutes
+			end := timeRange.end.timeInMinutes
+			_, err := domains.InitInterval(scheduler.Id, uint64(0), start, end, day.day)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
